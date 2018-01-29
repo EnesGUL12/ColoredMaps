968b76b37e2b206478f8186817ae038e9c476181
EnesGul <enes515254@gmail.com>
2018-01-29 23:05:23 +0600

    Create Node class and add map_create

---
+++ cmaps.py
@@ -0,0 +1,54 @@
+# Project: Colored maps, task from Charles Wetherell book
+#
+# https://github.com/EnesGUL12/ColoredMaps.git
+#
+# dr.doberman, EnesGUL, Faf_Faf
+#
+
+
+MAP_NODES = [
+    ["WA", 0, 0],
+    ["ID", 2, 0]
+]
+MAP_LINKS = {
+    "WA": ["OR", "ID"],
+    "ID": ["WA", "MT", "WY", "UT", "NV", "OR"]
+}
+
+class Node():
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.peers = []
+        self.color = -1
+    
+    def AddPeer(self, node):
+        if not self.IsLinked(node):
+            self.peers.append(node)
+        node.AddPeer(self)
+    
+    def IsLinked(self, node):
+        for n in self.peers:
+            if n.name == node.name:
+                return True
+        
+        return False
+
+
+
+def map_create():
+    map = dict()
+    # Пройти по MAP_NODES
+    for mi in MAP_NODES:
+        # Cоздать новый узел
+        map[mi[0]] = Node(mi[0], mi[1], mi[2])
+    
+    # Пройти по MAP_LINKS 
+    for n, l in MAP_LINKS:    
+        # Связать узлы
+        for ll in l:
+            map[n].AddPeer(map[ll])
+
+    return map
+    
